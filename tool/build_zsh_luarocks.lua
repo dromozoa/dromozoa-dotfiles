5dd609e870bf70bdb3f1ebe98a7f3224d3f4b5c0
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-01-23 14:01:22 +0900

    luarocksのzsh補完を修正。

--- tool/build_zsh_luarocks.lua
+++ tool/build_zsh_luarocks.lua
@@ -15,6 +15,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-dotfiles.  If not, see <http://www.gnu.org/licenses/>.
 
+local dumper = require "dromozoa.commons.dumper"
 local shell = require "dromozoa.commons.shell"
 
 local luarocks = ...
@@ -31,11 +32,74 @@ local function normalize_text(source)
   return source:gsub(path_pattern, "luarocks")
 end
 
+local function parse_help(command)
+  local state
+  local options = {}
+
+  local handle = assert(io.popen(("%s help %s"):format(shell.quote(luarocks), shell.quote(command))))
+  for line in handle:lines() do
+    if line:find "^%a" then
+      state = line
+    elseif state == "DESCRIPTION" then
+      local option, text = line:match "^\t(%-.-) +(.*)"
+      if option then
+        options[#options + 1] = { option, text }
+      else
+        local text = line:match "^\t +(.*)"
+        if text then
+          local option = options[#options]
+          option[2] = option[2] .. " " .. text
+        end
+      end
+    end
+  end
+  handle:close()
+
+  return options
+end
+
+local argument_map = {
+  server = "_hosts";
+}
+
+local function write_options(function_name, options)
+  local out = assert(io.open("zshfuncs/" .. function_name, "w"))
+  out:write(([[
+#autoload
+%s() {
+  options=(
+    $options
+]]):format(function_name))
+
+  for i = 1, #options do
+    local option = options[i]
+    local text = normalize_text(option[2])
+    local name, argument = option[1]:match "^(%-.-=)<(.-)>$"
+    local spec
+    if name then
+      local action = argument_map[argument]
+      if action then
+        spec = ("%s[%s]:%s:%s"):format(name, text, argument, action)
+      else
+        spec = ("%s[%s]:%s"):format(name, text, argument)
+      end
+    else
+      spec = ("%s[%s]"):format(option[1], text)
+    end
+    out:write("    ", shell.quote(spec), "\n")
+  end
+
+  out:write [[
+  )
+}
+]]
+end
+
 local state
 local general_options = {}
 local commands = {}
 
-local handle = assert(io.popen("luarocks help"))
+local handle = assert(io.popen(("%s help"):format(shell.quote(luarocks))))
 for line in handle:lines() do
   if line:find "^%a" then
     state = line
@@ -83,31 +147,11 @@ out:write [[
 }
 ]]
 
-local name = "__dromozoa_luarocks_general_options"
-local out = assert(io.open("zshfuncs/" .. name, "w"))
-out:write(([[
-#autoload
-%s() {
-  options=(
-    $options
-]]):format(name))
-for i = 1, #general_options do
-  local option = general_options[i]
-  local text = normalize_text(option[2])
-  local opt, arg = option[1]:match "^(%-.-=)<(.-)>$"
-  local spec
-  if opt then
-    if arg == "server" then
-      spec = opt .. "[" .. text .. "]:" .. arg .. ":_hosts"
-    else
-      spec = opt .. "[" .. text .. "]:" .. arg
-    end
-  else
-    spec = option[1] .. "[" .. text .. "]"
-  end
-  out:write("    ", shell.quote(spec), "\n")
-end
-out:write [[
-  )
-}
-]]
+write_options("__dromozoa_luarocks_general_options", general_options)
+
+local list_options = parse_help "list"
+write_options("__dromozoa_luarocks_list_options", list_options)
+
+local remove_options = parse_help "remove"
+write_options("__dromozoa_luarocks_remove_options", remove_options)
+print(dumper.encode(remove_options, { pretty = true }))
