ef3ccce9bfd98a3d9177af4e3937199a1b73a037
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-02-10 18:44:17 +0900

    ファイル名を変更。

--- tool/build_luarocks.lua
+++ tool/build_luarocks.lua
@@ -15,225 +15,325 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-dotfiles.  If not, see <http://www.gnu.org/licenses/>.
 
-local dumper = require "dromozoa.commons.dumper"
+local util = require "luarocks.util"
 local shell = require "dromozoa.commons.shell"
 
-local luarocks = ...
-if not luarocks then
-  luarocks = "luarocks"
+program_description = "(no program description)"
+
+util.this_program = function ()
+  return "luarocks"
 end
 
-local handle = assert(io.popen(("%s --version"):format(shell.quote(luarocks))))
-local path, version = handle:read():match"^(.-/luarocks) (%d[%d%.]+)$"
-handle:close()
-local path_pattern = path:gsub("%W", "%%%1")
-
-local function normalize_text(text)
-  text = text:gsub(path_pattern, "luarocks")
-  if text:find "%.%s" then
-    return text:match "^(.-)%.%s"
-  elseif text:find "%.$" then
-    return text:sub(1, -2)
-  else
-    return text
-  end
+local printout_buffer
+util.printout = function (...)
+  printout_buffer[#printout_buffer + 1] = table.concat({...}, "\t")
 end
 
-local function parse_help(command)
-  local state
-  local option_map = {}
-  local options = {}
+local deps_modes = {}
 
-  local handle = assert(io.popen(("%s help %s"):format(shell.quote(luarocks), shell.quote(command))))
-  for line in handle:lines() do
-    if line:find "^%a" then
-      state = line
-    elseif state == "SYNOPSIS" then
-      for opt in line:gmatch "%-[%a%-%=%<%>]+" do
-        option_map[opt] = true
-      end
-    elseif state == "DESCRIPTION" then
-      local opt, text = line:match "^\t(%-.-) +(.*)"
-      if opt then
-        option_map[opt] = nil
-        options[#options + 1] = { opt, text }
+local function parse_help(content)
+  local paragraphs = {}
+  local paragraph
+  local state = 1
+  for line in content:gmatch "[^\n]*" do
+    if line:find "^%s*%-%-" then
+      state = 2
+      paragraph = { (line:gsub("^%s*", "")) }
+      paragraphs[#paragraphs + 1] = paragraph
+    elseif state == 2 then
+      if line:find "^%s*$" then
+        state = 1
+        paragraph = nil
       else
-        local text = line:match "^\t +(.*)"
-        if text then
-          local option = options[#options]
-          if option then
-            option[2] = option[2] .. " " .. text
-          end
-        end
+        paragraph[#paragraph + 1] = line:gsub("^%s*", "")
       end
     end
   end
-  handle:close()
-
-  for i = 1, #options do
-    options[i][2] = normalize_text(options[i][2])
+  for i = 1, #paragraphs do
+    paragraphs[i] = table.concat(paragraphs[i], " ")
   end
+  return paragraphs
+end
+
+local function each_option(opts)
+  local names = {}
+  for name in pairs(opts) do
+    names[#names + 1] = name
+  end
+  table.sort(names)
+  return coroutine.wrap(function ()
+    for i = 1, #names do
+      coroutine.yield(opts[names[i]])
+    end
+  end)
+end
+
+local function make_option(opt)
+  local name = opt.name
+  local arg = opt.arg
+  local desc = opt.desc
+
+  local option = "--" .. opt.name
+  if arg ~= true then
+    arg = arg:gsub([[^"(.-)"$]], "%1")
+    arg = arg:gsub([[^%<(.-)%>$]], "%1")
+    option = option .. "="
+  end
+  if desc then
+    assert(not desc:find "]")
+    option = option .. "[" .. desc .. "]"
+  end
+  if arg ~= true then
+    option = option .. ":" .. arg
+  end
+  if arg == "mode" then
+    assert(name == "deps-mode")
+    option = option .. ":(("
+    for i = 1, #deps_modes do
+      local deps_mode = deps_modes[i]
+      local mode = deps_mode.mode
+      local desc = deps_mode.desc
+      if i > 1 then
+        option = option .. " "
+      end
+      assert(not desc:find [["]])
+      option = option .. mode .. [[\:"]] .. desc .. [["]]
+    end
+    option = option .. "))"
+  elseif arg == "file" or arg == "path" then
+    option = option .. ":_files"
+  elseif arg == "server" then
+    option = option .. ":_hosts"
+  elseif arg == "url" then
+    option = option .. ":_urls"
+  end
+  return option
+end
+
+local filename = assert(package.searchpath("luarocks.util", package.path))
+local handle = assert(io.open(filename))
+local content = handle:read "*a"
+handle:close()
+local chunk = "return " .. assert(content:match "\nlocal supported_flags = ({.-})\n")
+local supported_flags = assert(assert(load(chunk))())
+
+for item in os.getenv "PATH":gmatch "[^:]*" do
+  if item == "" then
+    item = "./luarocks"
+  else
+    item = item .. "/luarocks"
+  end
+  local handle = io.open(item)
+  if handle then
+    local script
+    for line in handle:lines() do
+      script = line:match [[^exec .- '([^']+)' "%$@"$]]
+      if script then
+        break
+      end
+    end
+    handle:close()
+    assert(script)
+
+    local handle = assert(io.open(script))
+    local content = handle:read "*a"
+    handle:close()
+    local chunk = "return " .. assert(content:match "\ncommands = ({.-})\n")
+    commands = assert(assert(load(chunk))())
+    break
+  end
+end
+
+local names = {}
+local modules = {}
+for name, module in pairs(commands) do
+  names[#names + 1] = name
+  modules[name] = require(module)
+end
+table.sort(names)
+
+printout_buffer = {}
+modules.help.command()
+
+local general_opts = {}
+
+local state = 1
+for i = 1, #printout_buffer do
+  local buffer = printout_buffer[i]
+  if buffer == "\nGENERAL OPTIONS" then
+    state = 2
+  elseif buffer == "\nVARIABLES" then
+    state = 1
+  elseif state >= 2 then
+    local paragraphs = parse_help(buffer)
+    for j = 1, #paragraphs do
+      local paragraph = paragraphs[j]
+      local name, desc = paragraph:match "^%-%-(%S+)%s+(.*)$"
+      name = name:gsub("=.*", "")
+      general_opts[name] = {
+        name = name;
+        arg = assert(supported_flags[name]);
+        desc = desc;
+      }
+    end
+  end
+end
+
+for i = 1, #names do
+  local name = names[i]
+  local module = modules[name]
+  local arguments = module.help_arguments
+  if arguments == nil then
+    arguments = "<argument>"
+  end
+  local help = module.help
 
   local opts = {}
-  for opt in pairs(option_map) do
-    opts[#opts + 1] = opt
-  end
-  table.sort(opts)
-  for i = 1, #opts do
-    options[#options + 1] = { opts[i] }
+  local args = {}
+  module.opts = opts
+  module.args = args
+
+  for item in arguments:gmatch "[%w%-<=>_]+" do
+    local name = item:match "^%-%-([%w%-]+)"
+    if name then
+      opts[name] = {
+        name = name;
+        arg = assert(supported_flags[name]);
+      }
+    else
+      args[#args + 1] = assert(item:match "^<([^>]*)>$")
+    end
   end
 
-  return options
+  local paragraphs = parse_help(help)
+  for j = 1, #paragraphs do
+    local paragraph = paragraphs[j]
+    paragraph = paragraph:gsub("^(%-%-deps%-mode.-%.).*", "%1")
+    local name, desc = paragraph:match "^%-%-(%S+)%s+(.*)$"
+    name = name:gsub("=.*", "")
+    opts[name] = {
+      name = name;
+      arg = assert(supported_flags[name]);
+      desc = desc;
+    }
+  end
 end
 
-local argument_map = {
-  server = "_hosts";
-  mode = "(all one order none)";
-}
+local indent
+for line in util.deps_mode_help():gmatch "[^\n]*" do
+  local sp, mode, desc = line:match "^( +)%* (%w+) %- (.*)"
+  if sp then
+    indent = #sp + 2
+    deps_modes[#deps_modes + 1] = {
+      mode = mode;
+      desc = desc;
+    }
+  else
+    local sp, desc = line:match "^( +)(.*)"
+    if sp and #sp == indent then
+      local deps_mode = deps_modes[#deps_modes]
+      deps_mode.desc = deps_mode.desc .. " " .. desc
+    end
+  end
+end
 
-local function write_options(function_name, options)
-  local out = assert(io.open("zshfuncs/" .. function_name, "w"))
-  out:write(([[
+local out = assert(io.open("zshfuncs/_dromozoa_luarocks_options", "w"))
+out:write [[
 #autoload
-%s() {
+_dromozoa_luarocks_options() {
   options=(
     $options
-]]):format(function_name))
+]]
 
-  for i = 1, #options do
-    local option = options[i]
-    local opt = option[1]
-    local text = option[2]
-    if text then
-      text = ("[%s]"):format(text)
-    else
-      text = ""
-    end
-    local name, argument = opt:match "^(%-.-=)<(.-)>$"
-    local spec
-    if name then
-      local action = argument_map[argument]
-      if action then
-        spec = ("%s%s:%s:%s"):format(name, text, argument, action)
-      else
-        spec = ("%s%s:%s"):format(name, text, argument)
-      end
-    else
-      spec = ("%s%s"):format(option[1], text)
-    end
-    out:write("    ", shell.quote(spec), "\n")
-  end
+for opt in each_option(general_opts) do
+  out:write(([[
+    %s
+]]):format(shell.quote(make_option(opt))))
+end
 
-  out:write [[
+out:write [[
   )
+
+  case x$1 in
+]]
+
+for i = 1, #names do
+  local name = names[i]
+  local module = modules[name]
+  local opts = module.opts
+  if next(opts) ~= nil then
+    out:write(([[
+    x%s)
+      options=(
+        $options
+]]):format(name))
+
+    for opt in each_option(opts) do
+      out:write(([[
+        %s
+]]):format(shell.quote(make_option(opt))))
+    end
+
+      out:write [[
+      )
+      ;;
+]]
+  end
+end
+
+out:write [[
+  esac
 }
 ]]
-end
 
-local state
-local general_options = {}
-local commands = {}
+out:close()
 
-local handle = assert(io.popen(("%s help"):format(shell.quote(luarocks))))
-for line in handle:lines() do
-  if line:find "^%a" then
-    state = line
-  elseif state == "GENERAL OPTIONS" then
-    local opt, text = line:match "^\t(%-.-) +(.*)"
-    if opt then
-      general_options[#general_options + 1] = { opt, text }
-    else
-      local text = line:match "^\t +(.*)"
-      if text then
-        local option = general_options[#general_options]
-        option[2] = option[2] .. " " .. text
-      end
-    end
-  elseif state == "COMMANDS" then
-    local indent, text = line:match "^(\t+)(.*)"
-    if indent then
-      if #indent == 1 then
-        commands[#commands + 1] = { text }
-      else
-        assert(#indent == 2)
-        local command = commands[#commands]
-        assert(#command == 1)
-        command[2] = text
-      end
-    end
-  end
-end
-handle:close()
-
-for i = 1, #general_options do
-  general_options[i][2] = normalize_text(general_options[i][2])
-end
-
-for i = 1, #commands do
-  commands[i][2] = normalize_text(commands[i][2])
-end
-
-local name = "_dromozoa_luarocks_commands"
-local out = assert(io.open("zshfuncs/" .. name, "w"))
-out:write(([[
+local out = assert(io.open("zshfuncs/_dromozoa_luarocks_commands", "w"))
+out:write [[
 #autoload
-%s() {
-  commands=(
-    $commands
-]]):format(name))
-for i = 1, #commands do
-  local command = commands[i]
-  out:write("    ", shell.quote(command[1] .. ":" .. command[2]), "\n")
+_dromozoa_luarocks_commands() {
+  arguments=(
+    $arguments
+]]
+
+for i = 1, #names do
+  local name = names[i]
+  local module = modules[name]
+  local summary = assert(module.help_summary)
+  out:write(([[
+    %s
+]]):format(shell.quote(name .. ":" .. summary)))
 end
+
 out:write [[
   )
 }
 ]]
 
-write_options("_dromozoa_luarocks_general_options", general_options)
+out:close()
 
-local options = parse_help "config"
-write_options("_dromozoa_luarocks_config_options", options)
+local out = assert(io.open("zshfuncs/_dromozoa_luarocks_arguments", "w"))
+out:write [[
+#autoload
+_dromozoa_luarocks_arguments() {
+  case x$1 in
+]]
 
-local options = parse_help "doc"
-write_options("_dromozoa_luarocks_doc_options", options)
+for i = 1, #names do
+  local name = names[i]
+  local module = modules[name]
+  local args = module.args
+  if next(args) ~= nil then
+    out:write(([[
+    x%s)
+      arguments=($arguments %s);;
+]]):format(name, shell.quote(": :_luarocks_arguments " .. table.concat(args, " "))))
+  end
+end
 
-local options = parse_help "download"
-write_options("_dromozoa_luarocks_download_options", options)
+out:write [[
+  esac
+}
+]]
 
-local options = parse_help "install"
-write_options("_dromozoa_luarocks_install_options", options)
-
--- lint
-
-local options = parse_help "list"
-write_options("_dromozoa_luarocks_list_options", options)
-
-local options = parse_help "make"
-write_options("_dromozoa_luarocks_make_options", options)
-
--- new_version
--- pack
-
-local options = parse_help "path"
-write_options("_dromozoa_luarocks_path_options", options)
-
-local options = parse_help "purge"
-write_options("_dromozoa_luarocks_purge_options", options)
-
-local options = parse_help "remove"
-write_options("_dromozoa_luarocks_remove_options", options)
-
-local options = parse_help "search"
-write_options("_dromozoa_luarocks_search_options", options)
-
-local options = parse_help "show"
-write_options("_dromozoa_luarocks_show_options", options)
-
-local options = parse_help "unpack"
-write_options("_dromozoa_luarocks_unpack_options", options)
-
-local options = parse_help "upload"
-write_options("_dromozoa_luarocks_upload_options", options)
-
--- write_rockspec
+out:close()
