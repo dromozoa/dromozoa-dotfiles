e533208f5ccaa4a6c67d5d11fd9a078cb9b1454f
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-17 16:35:24 +0900

    画像の実験を修正。

--- tool/image.lua
+++ tool/image.lua
@@ -22,33 +22,24 @@ local png = require "dromozoa.png"
 local mode, filename = ...
 
 if mode == "char" then
+  io.write [[
+| code   | eaw | chars    |
+|--------|-----|----------|
+]]
   for code = 0x2580, 0x259F do
     local char = utf8.char(code)
-    io.write(("U+%04X %2s %s%s%s%s\n"):format(code, ucd.east_asian_width(code), char, char, char, char))
+    local eaw = ucd.east_asian_width(code)
+    assert(eaw == "A" or eaw == "N")
+    local chars
+    if eaw == "A" then
+      chars = char:rep(4)
+    else
+      chars = char:rep(8)
+    end
+    io.write(("| U+%04X |  %-2s | %s |\n"):format(code, eaw, chars))
   end
   os.exit()
 end
-
---[[
-local TL = utf8.char(0x2598)
-local TR = utf8.char(0x259D)
-local BL = utf8.char(0x2596)
-local BR = utf8.char(0x2597)
-
-local chars = { TL, TR, BL, BR }
-for i = 1, #chars do
-  local char = chars[i]
-  io.write(char, char, char, char)
-  if i == #chars then
-    io.write "\n"
-  else
-    io.write "\27[4D"
-  end
-end
-
-local UPPER_HALF_BLOCK = utf8.char(0x2580)
-local LOWER_HALF_BLOCK = utf8.char(0x2584)
-]]
 
 local reader = assert(png.reader())
 local handle = assert(io.open(filename, "rb"))
@@ -60,12 +51,12 @@ assert(reader:read_png(png.PNG_TRANSFORM_EXPAND + (png.PNG_TRANSFORM_SCALE_16 or
 
 local width = assert(reader:get_image_width())
 local height = assert(reader:get_image_height())
-local pixel = " "
-if width == height then
-  pixel = "  "
-end
 
 if mode == "24bit" then
+  local pixel = " "
+  if width == height then
+    pixel = "  "
+  end
   for y = 1, height do
     local row = assert(reader:get_row(y))
     for i = 1, width * 4, 4 do
@@ -75,14 +66,51 @@ if mode == "24bit" then
     io.write "\27[0m\n"
   end
 elseif mode == "8bit" then
+  local pixel = " "
+  if width == height then
+    pixel = "  "
+  end
+  local d = 256 / 6
   for y = 1, height do
     local row = assert(reader:get_row(y))
     for i = 1, width * 4, 4 do
       local r, g, b = row:byte(i, i + 2)
-      r = math.floor(r * 6 / 256)
-      g = math.floor(g * 6 / 256)
-      b = math.floor(b * 6 / 256)
+      r = math.floor(r / d)
+      g = math.floor(g / d)
+      b = math.floor(b / d)
       io.write("\27[48;5;", 16 + r * 36 + 6 * g + b, "m", pixel)
+    end
+    io.write "\27[0m\n"
+  end
+elseif mode == "8bit-tile" then
+  local pixel = utf8.char(0x259A)
+  if width == height then
+    pixel = pixel .. pixel
+  end
+  local d = 256 / 6
+  local function e(u, v)
+    local a = u - v
+    if a < -0.5 and v > 1 then
+      return v - 1
+    elseif a > 0.5 and v < 5 then
+      return v + 1
+    else
+      return v
+    end
+  end
+  for y = 1, height do
+    local row = assert(reader:get_row(y))
+    for i = 1, width * 4, 4 do
+      local r, g, b = row:byte(i, i + 2)
+      local r1 = math.floor(r / d)
+      local g1 = math.floor(g / d)
+      local b1 = math.floor(b / d)
+      local fg = 16 + r1 * 36 + 6 * g1 + b1
+      local r2 = e(r, r1)
+      local g2 = e(g, g1)
+      local b2 = e(b, b1)
+      local bg = 16 + r2 * 36 + 6 * g2 + b2
+      io.write("\27[38;5;", fg, ";48;5;", bg, "m", pixel)
     end
     io.write "\27[0m\n"
   end
