72c820f3c1b1700696771be720cd687946577e2c
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-26 08:15:16 +0900

    著作権表記を更新。

--- docs/jlreq.js
+++ docs/jlreq.js
@@ -9,11 +9,11 @@
 //
 // dromozoa-dotfiles is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with dromozoa-dotfiles.  If not, see <http://www.gnu.org/licenses/>.
+// along with dromozoa-dotfiles. If not, see <https://www.gnu.org/licenses/>.
 
 // https://www.w3.org/TR/jlreq/
 // https://www.w3.org/TR/jlreq/ja/
