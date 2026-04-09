19eb595e8e46ea3ed0d3b345a1a65339efa31f3d
Tomoyuki Fujimori <moyu@dromozoa.com>
2026-04-09 15:44:00 +0900

    neovimの設定を整理。

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,3 @@
--- 旧: set runtimepath^=~/dromozoa-dotfiles/vimfiles
--- vim.opt.runtimepath:prepend(vim.fn.expand("~/dromozoa-dotfiles/vimfiles"))
-
--- 旧: let g:netrw_home="~/.vim"
--- vim.g.netrw_home = vim.fn.expand("~/.vim")
-
--- 自作Luaモジュールを置くなら、将来的には
--- ~/.config/nvim/lua/ 以下へ寄せるほうが自然。
--- 旧互換で外部ディレクトリを使うなら package.path を調整する。
--- package.path = table.concat({
---   vim.fn.expand("~/dromozoa-dotfiles/?.lua"),
---   package.path,
--- }, ";")
-
--- -------------------------------------------------------------------
--- options
--- -------------------------------------------------------------------
-
 require "lsp"
 
 local opt = vim.opt
@@ -36,10 +18,10 @@ opt.breakindent = true
 opt.number = true
 opt.list = true
 opt.listchars = {
-  tab = "__",
-  trail = "_",
-  precedes = "<",
-  extends = ">",
+  tab = "__";
+  trail = "_";
+  precedes = "<";
+  extends = ">";
 }
 opt.laststatus = 2
 opt.statusline = [[%F\ %m%r%h%w%y%{'['.&fileencoding.']['.&fileformat.']'}%=[%l,%c][U+%04B]]
@@ -48,9 +30,7 @@ opt.hlsearch = true
 opt.ignorecase = true
 opt.incsearch = true
 opt.smartcase = true
-
 opt.autowrite = true
-opt.updatetime = 200
 
 opt.debug = "msg"
 opt.clipboard = "unnamed"
@@ -58,76 +38,16 @@ opt.modeline = true
 opt.modelines = 5
 opt.wildmode = { "list", "longest" }
 
--- -------------------------------------------------------------------
--- runtime features
--- -------------------------------------------------------------------
-
 vim.cmd "packadd! matchit"
 vim.cmd "syntax enable"
 vim.cmd "filetype plugin on"
-
--- -------------------------------------------------------------------
--- colors / highlight
--- -------------------------------------------------------------------
 
 vim.cmd "colorscheme darkblue"
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "Underlined", { ctermfg = "LightBlue" })
 
--- -------------------------------------------------------------------
--- autocommands
--- -------------------------------------------------------------------
-
-local wall_group = vim.api.nvim_create_augroup("wall", { clear = true })
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = wall_group,
-  pattern = "*",
-  command = "silent! wall",
-})
-vim.api.nvim_create_autocmd("CursorHold", {
-  group = wall_group,
-  pattern = "*",
-  command = "silent! wall",
-})
-
-local ft_group = vim.api.nvim_create_augroup("filetype_local_options", { clear = true })
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = ft_group,
-  pattern = "html",
-  callback = function()
-    vim.opt_local.wrap = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = ft_group,
-  pattern = "markdown",
-  callback = function()
-    vim.opt_local.wrap = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = ft_group,
-  pattern = "text",
-  callback = function()
-    vim.opt_local.textwidth = 60
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = ft_group,
-  pattern = "lua",
-  callback = function()
-    vim.cmd("syntax sync minlines=500 maxlines=1000")
-  end,
-})
-
-local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
-
-map("n", "j", "gj", opts)
-map("n", "k", "gk", opts)
-map("n", "<C-p>", "<Cmd>bprev<CR>", opts)
-map("n", "<C-n>", "<Cmd>bnext<CR>", opts)
+vim.keymap.set("n", "j", "gj", opts)
+vim.keymap.set("n", "k", "gk", opts)
+vim.keymap.set("n", "<C-p>", "<Cmd>bprev<CR>", opts)
+vim.keymap.set("n", "<C-n>", "<Cmd>bnext<CR>", opts)
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -3,22 +3,22 @@ local function lsp_buf_map(bufnr, lhs, rhs, desc, mode)
 end
 
 vim.diagnostic.config({
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+  underline = true;
+  update_in_insert = false;
+  severity_sort = true;
   virtual_text = {
-    spacing = 2,
-    source = "if_many",
-  },
-  signs = true,
+    spacing = 2;
+    source = "if_many";
+  };
+  signs = true;
   float = {
-    border = "rounded",
-    source = "if_many",
-  },
+    border = "rounded";
+    source = "if_many";
+  };
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("my-lsp-attach", { clear = true }),
+  group = vim.api.nvim_create_augroup("my-lsp-attach", { clear = true });
   callback = function(args)
     local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
@@ -47,22 +47,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
       local group = vim.api.nvim_create_augroup("my-lsp-highlight-" .. bufnr, { clear = true })
 
       vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        group = group,
-        buffer = bufnr,
-        callback = vim.lsp.buf.document_highlight,
+        group = group;
+        buffer = bufnr;
+        callback = vim.lsp.buf.document_highlight;
       })
 
       vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "LspDetach" }, {
-        group = group,
-        buffer = bufnr,
-        callback = vim.lsp.buf.clear_references,
+        group = group;
+        buffer = bufnr;
+        callback = vim.lsp.buf.clear_references;
       })
     end
 
     if client:supports_method("textDocument/inlayHint") then
       vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
     end
-  end,
+  end;
 })
 
 vim.lsp.config("lua_ls", {
