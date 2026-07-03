fed5be790fd46f941075386ea8959e5a85fec664
Tomoyuki Fujimori <moyu@dromozoa.com>
2026-07-03 11:33:43 +0900

    スクリプトを更新。

--- test/zsphre/zsphre-test.sh
+++ test/zsphre/zsphre-test.sh
@@ -1,24 +1,36 @@
 #! /bin/sh -e
 
-db_file=zsphre-test.db
+db_file=test.db
 
-sqlite3 -header -csv "$db_file" <<EOH
-pragma auto_vacuum=INCREMENTAL;
-pragma journal_mode=WAL;
+if ! test -f "$db_file"
+then
+  sqlite3 -csv "$db_file" <create.sql >/dev/null
+fi
 
-create table if not exists commands (
-  id integer primary key,
-  started_at text not null,
-  finished_at text,
-  hist text not null,
-  line text not null,
-  full text not null,
-  cwd text not null,
-  tty text not null,
-  host text not null,
-  status integer,
-  pipe_status text,
-  on_finish text
-);
+id=$(sqlite3 -csv "$db_file" <insert.sql)
+
+sqlite3 -csv "$db_file" <<EOH >/dev/null
+begin immediate transaction;
+
+update commands
+set finished_at = strftime('%Y-%m-%dT%H:%M:%fZ'), status = 0, pipe_status = '0'
+where id = $id;
+
+select
+  strftime('%Y/%m/%d %H:%M:%S', started_at, 'localtime') as started_at,
+  strftime('%Y/%m/%d %H:%M:%S', finished_at, 'localtime') as finished_at,
+  strftime('%s', finished_at) - strftime('%s', started_at) as elapsed,
+  hist,
+  line,
+  full,
+  cwd,
+  tty,
+  host,
+  status,
+  pipe_status,
+  on_finish
+from commands
+where id = $id;
+
+commit transaction;
 EOH
-
